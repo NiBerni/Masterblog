004eb0ba824507381f4ada96eedb9ddab684765d
Berni <277801407+NiBerni@users.noreply.github.com>
2026-06-24 13:37:16 +0200

    feat(blog): Integrate blog blueprint and display posts dynamically
    *   Register `blog_bp` blueprint in `app.py`.
    *   Update `index.html` to dynamically render blog posts.
    
    Signed-off-by: Berni <277801407+NiBerni@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask
 
+from blueprints.blog import blog_bp
+
 app = Flask(__name__)
 
+app.register_blueprint(blog_bp)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
