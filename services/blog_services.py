dbafb59a9caeb29e7aa287d62039b3c486af490d
Berni <277801407+NiBerni@users.noreply.github.com>
2026-06-24 14:07:58 +0200

    feat(blog): Implement functionality to add new posts
    *   Add `add_post` function to `blog_services.py`.
    
    Signed-off-by: Berni <277801407+NiBerni@users.noreply.github.com>

--- services/blog_services.py
+++ services/blog_services.py
@@ -22,3 +22,31 @@ def get_all_posts() -> list:
             return json.load(file)
         except json.JSONDecodeError:
             return []
+
+
+def add_post(author: str, title: str, content: str) -> dict:
+    """
+    Adds a new blog post to the JSON storage file.
+
+    Reads existing posts, generates a new unique ID based on the highest existing ID,
+    appends the new post, and saves the updated list back to the file.
+
+    :param author: The author of the blog post.
+    :param title: The title of the blog post.
+    :param content: The content of the blog post.
+    :return: The newly created blog post dictionary.
+    :rtype: dict
+    """
+    posts = get_all_posts()
+    # Generate a new unique ID based on the max ID currently in the file
+    new_id = 1
+    if posts:
+        new_id = max(post.get("id", 0) for post in posts) + 1
+
+    new_post = {"id": new_id, "author": author, "title": title, "content": content}
+    posts.append(new_post)
+
+    with open(DATA_FILE_PATH, "w", encodinf="utf-8") as file:
+        json.dump(posts, file, indent=4)
+
+    return new_post
